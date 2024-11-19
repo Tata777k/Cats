1ced1ff7ebfa8eba6a724dbe061be00ae810febe
Tatyana Kos <kosttata777@gmail.com>
2024-11-19 21:53:26 +0300

    create window and vidget

---
+++ main.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+window = TK()
+window.title("Cats.")
+window.geometry("600x480")
+
+
+label = Label()
+label.pack()
+
+url = "https://cataas.com/cat"
+img = load_image(url)
+
+if img:
+  label.config(image=img)
+  label.image = img
+
+
+window.mainloop()
