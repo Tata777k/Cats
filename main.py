14aa2f3262b917cdbb66408c362b367e643daf5f
Tatyana Kos <kosttata777@gmail.com>
2024-11-20 20:42:46 +0300

    add combobox for select a tag

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from tkinter import *
+from  tkinter import ttk
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+Allowed_tags = ["sleep", "jump", "Fluffy", "fight", "black", "white", "bucket", "feet", "siamese", "cake"]
 
 def load_image(url):
     try:
@@ -18,7 +20,7 @@ def load_image(url):
 
 
 def open_new_window():
-    tag = tag_entry.get()
+    tag = tag_combobox.get() # = tag_entry.get()
     url_tag = f"https://cataas.com/cat/{tag}" if tag else "https://cataas.com/cat"
     img = load_image(url_tag)
 
@@ -37,12 +39,6 @@ window = Tk()
 window.title("Cats.")
 window.geometry("600x550")
 
-tag_entry = Entry()
-tag_entry.pack()
-
-
-load_button = Button(text="Загрузить по тегу", command=open_new_window)
-load_button.pack()
 
 menu_b = Menu(window)
 window.config(menu=menu_b)
@@ -55,5 +51,12 @@ file_menu.add_command(label="Выход", command=exit)
 
 url = "https://cataas.com/cat"
 
+tag_label = Label(text="Выбери тег")
+tag_label.pack()
 
+tag_combobox = ttk.Combobox(values=Allowed_tags)
+tag_combobox.pack()
+
+load_button = Button(text="Загрузить по тегу", command=open_new_window)
+load_button.pack()
 window.mainloop()
